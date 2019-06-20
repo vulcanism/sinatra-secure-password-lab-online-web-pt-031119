6c663714bd79a16b613360b6c13afd848f28302c
Savannah C <sgcorrell@gmail.com>
2019-06-20 15:59:13 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    user = User.new()
+    user = User.new(:username => params[:username], :password => params[:password])
   end
 
   get '/account' do
